5f715d574226f4d1513cdfd5055bbbd9a90a2fab
me8927 <malachi.espinola@gmail.com>
2024-12-19 12:03:59 -0800

    Update card_inventory.py

--- card_inventory.py
+++ card_inventory.py
@@ -298,9 +298,9 @@ def run_top_to_bottom():
             if st.session_state.print_barcodes_in_viewing:
                 def save_to_print_queue():
                     st.session_state.print_barcodes_in_viewing = False
-                    #st.session_state.barcode_print_list.append([quantity_to_print, f"{st.session_state.current_copy.loc['id']}"])
-                    for j in filtered_data['id'].tolist():
-                        st.session_state.barcode_print_list.append([1, j])
+                    st.session_state.barcode_print_list.append([quantity_to_print, f"{st.session_state.current_copy.loc['id']}"])
+                    # for j in filtered_data['id'].tolist():
+                    #     st.session_state.barcode_print_list.append([1, j])
 
                 def cancel_print():
                     st.session_state.print_barcodes_in_viewing = False
